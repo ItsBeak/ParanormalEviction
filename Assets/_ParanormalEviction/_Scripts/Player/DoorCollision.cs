6e63341256f42a8006ac8fe763dd7f1b5d748ea0
Tom Collyer <karzacinc@gmail.com>
2022-11-23 15:58:18 +1100

    Updated Main Menu, Light Mapping & Bug Fixes

---
+++ Assets/_ParanormalEviction/_Scripts/Player/DoorCollision.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorCollision : MonoBehaviour
+{
+
+    public FloatEditor Test;
+
+    public void Toggle()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        
+        Debug.Log("An object is still inside of the trigger");
+        
+
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+}
