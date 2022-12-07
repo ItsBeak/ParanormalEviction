c5444db188af6fe722a490c2574e711b1afbb531
Lochlan.R <lrobichaud666@gmail.com>
2022-12-07 17:29:03 +1100

    Cross Platform Compiling
    Trying to get all the cross platform stuff together for submission.

---
+++ Assets/_ParanormalEviction/_Scripts/SwapGameUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SwapGameUI : MonoBehaviour
+{
+    public GameObject GameUI;
+    public GameObject MobileUI;
+
+    public Image timerFill;
+    public Transform timerHand;
+    public Text guestDisplay;
+    // Start is called before the first frame update
+#if UNITY_ANDROID
+  void Start()
+    {
+        GameManager Access = FindObjectOfType<GameManager>();
+        Access.timerHand = timerHand;
+        Access.guestDisplay = guestDisplay;
+        Access.timerFill = timerFill;
+        MobileUI.SetActive(true);
+        GameUI.SetActive(false);
+    }
+
+#endif
+}
